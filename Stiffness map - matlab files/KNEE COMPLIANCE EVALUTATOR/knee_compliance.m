cc6b99f211d2de890342e6610742cc2d5c6c5da5
Lorenzo Capetta <lorenzo.capetta@unibo.it>
2026-09-21 12:24:04 +0200

    name update
    name changed from compliance to response_to_load

--- Stiffness map - matlab files/KNEE COMPLIANCE EVALUTATOR/knee_compliance.m
+++ Stiffness map - matlab files/KNEE COMPLIANCE EVALUTATOR/knee_compliance.m
@@ -1,4 +1,4 @@
-function [D_GeS] = knee_compliance(flexion_angle,M,F,V,varargin)
+function [D_GeS] = knee_response_to_load(flexion_angle,M,F,V,varargin)
 % This function return the variation in femur position and orientation with
 % respect to the values observed for the knee natural motion at the
 % considered flexion_angle.
@@ -6,7 +6,7 @@ function [D_GeS] = knee_compliance(flexion_angle,M,F,V,varargin)
 % induced by each load component separately.
 %
 %INPUT
-% flexion_angle = the considered knee flexion angle, [°]
+% flexion_angle = the considered knee flexion angle, [Â°]
 % M = the torque vector (1X3) applied to the femur, expressed in the tibial
 % anatomical reference system, [Nm]. The z component is assumed to be null
 % since the knee is assumed to provide no resistance to flexion.
@@ -29,7 +29,7 @@ function [D_GeS] = knee_compliance(flexion_angle,M,F,V,varargin)
 
 % check
 if nargin < 3
-    error('Not enough input arguments in "knee_compliance"')
+    error('Not enough input arguments in "knee_response_to_load"')
     % V is the only optional input
 elseif nargin < 4
     % If V is not given, set V to a default value (Volume of the map
@@ -77,39 +77,39 @@ else
 end
 
 % rotations and displacements due to Mx
-D_Mx(1) = compliance_fitting_model(CMx(1,:),flexion_angle,M(1));
-D_Mx(2) = compliance_fitting_model(CMx(2,:),flexion_angle,M(1));
-D_Mx(3) = compliance_fitting_model(CMx(3,:),flexion_angle,M(1));
-D_Mx(4) = compliance_fitting_model(CMx(4,:),flexion_angle,M(1));
-D_Mx(5) = compliance_fitting_model(CMx(5,:),flexion_angle,M(1));
+D_Mx(1) = response_to_load_fitting_model(CMx(1,:),flexion_angle,M(1));
+D_Mx(2) = response_to_load_fitting_model(CMx(2,:),flexion_angle,M(1));
+D_Mx(3) = response_to_load_fitting_model(CMx(3,:),flexion_angle,M(1));
+D_Mx(4) = response_to_load_fitting_model(CMx(4,:),flexion_angle,M(1));
+D_Mx(5) = response_to_load_fitting_model(CMx(5,:),flexion_angle,M(1));
 
 % rotations and displacements due to My
-D_My(1) = compliance_fitting_model(CMy(1,:),flexion_angle,M(2));
-D_My(2) = compliance_fitting_model(CMy(2,:),flexion_angle,M(2));
-D_My(3) = compliance_fitting_model(CMy(3,:),flexion_angle,M(2));
-D_My(4) = compliance_fitting_model(CMy(4,:),flexion_angle,M(2));
-D_My(5) = compliance_fitting_model(CMy(5,:),flexion_angle,M(2));
+D_My(1) = response_to_load_fitting_model(CMy(1,:),flexion_angle,M(2));
+D_My(2) = response_to_load_fitting_model(CMy(2,:),flexion_angle,M(2));
+D_My(3) = response_to_load_fitting_model(CMy(3,:),flexion_angle,M(2));
+D_My(4) = response_to_load_fitting_model(CMy(4,:),flexion_angle,M(2));
+D_My(5) = response_to_load_fitting_model(CMy(5,:),flexion_angle,M(2));
 
 % rotations and displacements due to Fx
-D_Fx(1) = compliance_fitting_model(CFx(1,:),flexion_angle,F(1));
-D_Fx(2) = compliance_fitting_model(CFx(2,:),flexion_angle,F(1));
-D_Fx(3) = compliance_fitting_model(CFx(3,:),flexion_angle,F(1));
-D_Fx(4) = compliance_fitting_model(CFx(4,:),flexion_angle,F(1));
-D_Fx(5) = compliance_fitting_model(CFx(5,:),flexion_angle,F(1));
+D_Fx(1) = response_to_load_fitting_model(CFx(1,:),flexion_angle,F(1));
+D_Fx(2) = response_to_load_fitting_model(CFx(2,:),flexion_angle,F(1));
+D_Fx(3) = response_to_load_fitting_model(CFx(3,:),flexion_angle,F(1));
+D_Fx(4) = response_to_load_fitting_model(CFx(4,:),flexion_angle,F(1));
+D_Fx(5) = response_to_load_fitting_model(CFx(5,:),flexion_angle,F(1));
 
 % rotations and displacements due to Fy
-D_Fy(1) = compliance_fitting_model(CFy(1,:),flexion_angle,F(2));
-D_Fy(2) = compliance_fitting_model(CFy(2,:),flexion_angle,F(2));
-D_Fy(3) = compliance_fitting_model(CFy(3,:),flexion_angle,F(2));
-D_Fy(4) = compliance_fitting_model(CFy(4,:),flexion_angle,F(2));
-D_Fy(5) = compliance_fitting_model(CFy(5,:),flexion_angle,F(2));
+D_Fy(1) = response_to_load_fitting_model(CFy(1,:),flexion_angle,F(2));
+D_Fy(2) = response_to_load_fitting_model(CFy(2,:),flexion_angle,F(2));
+D_Fy(3) = response_to_load_fitting_model(CFy(3,:),flexion_angle,F(2));
+D_Fy(4) = response_to_load_fitting_model(CFy(4,:),flexion_angle,F(2));
+D_Fy(5) = response_to_load_fitting_model(CFy(5,:),flexion_angle,F(2));
 
 % rotations and displacements due to Fy
-D_Fz(1) = compliance_fitting_model(CFz(1,:),flexion_angle,F(3));
-D_Fz(2) = compliance_fitting_model(CFz(2,:),flexion_angle,F(3));
-D_Fz(3) = compliance_fitting_model(CFz(3,:),flexion_angle,F(3));
-D_Fz(4) = compliance_fitting_model(CFz(4,:),flexion_angle,F(3));
-D_Fz(5) = compliance_fitting_model(CFz(5,:),flexion_angle,F(3));
+D_Fz(1) = response_to_load_fitting_model(CFz(1,:),flexion_angle,F(3));
+D_Fz(2) = response_to_load_fitting_model(CFz(2,:),flexion_angle,F(3));
+D_Fz(3) = response_to_load_fitting_model(CFz(3,:),flexion_angle,F(3));
+D_Fz(4) = response_to_load_fitting_model(CFz(4,:),flexion_angle,F(3));
+D_Fz(5) = response_to_load_fitting_model(CFz(5,:),flexion_angle,F(3));
 
 % Displacement is obtained as the linar combination of the displacement
 % induced by each load component separately.
