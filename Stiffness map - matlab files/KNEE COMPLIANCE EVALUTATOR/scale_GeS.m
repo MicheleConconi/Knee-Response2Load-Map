cf7d625c7de4b1af06cf11b397c6e7d885cf6ff3
Lorenzo Capetta <lorenzo.capetta@unibo.it>
2026-09-21 12:34:05 +0200

    Update scale_GeS.m
    grammatical corrections

--- Stiffness map - matlab files/KNEE COMPLIANCE EVALUTATOR/scale_GeS.m
+++ Stiffness map - matlab files/KNEE COMPLIANCE EVALUTATOR/scale_GeS.m
@@ -6,16 +6,16 @@ function [scaled_GeS] = scale_GeS(GeS,V,varargin)
 %
 %INPUT
 % GeS: the pose of the femur relative to the tibia, or vice versa. Unscaled
-% (output of the compliance pose calculation). Rotation are in degree,
-% translation in mm. The vector contain in the order:
+% (output of the compliance pose calculation). Rotations are in degrees,
+% translations in mm. The vector contains in order:
 % [flexion_angle, AA, IE, X, Y, Z]
 %
 % V: volume of the femur of the current subject under analysis
 %
 %OUTPUT
 % scaled_GeS: the pose of the femur relative to the tibia, or vice versa.
-% Scaled to the current subject under analysis. Rotation are in degree,
-% translation in mm. The vector contain in the order:
+% Scaled to the current subject under analysis. Rotations are in degrees,
+% translations in mm. The vector contains in order:
 % [flexion_angle, AA, IE, X, Y, Z]
 
 V_reference = 548666.453; % [mm^3]
