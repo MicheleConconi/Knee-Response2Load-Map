6764c5d287580db2dd522c9ac1261102ff87c365
Lorenzo Capetta <lorenzo.capetta@unibo.it>
2026-09-21 12:34:47 +0200

    Update knee_loaded_motion.m
    grammatical corrections

--- Stiffness map - matlab files/KNEE COMPLIANCE EVALUTATOR/knee_loaded_motion.m
+++ Stiffness map - matlab files/KNEE COMPLIANCE EVALUTATOR/knee_loaded_motion.m
@@ -38,7 +38,7 @@ function [GeS,NGeS,DGeS] = knee_loaded_motion(flexion_angle_in, M_in, F_in, movi
 %
 %OUTPUT
 % GeS: the pose of the femur relative to the tibia at flexion_angle_in. 
-% Rotations are in degrees, translations in mm. The vector is contained in the
+% Rotations are in degrees, translations in mm. The vector contains in
 % order: [flexion_angle, AA, IE, X, Y, Z]
 % In the case of mapping the motion of the tibia with respect to the femur, 
 % no sing correction is applied
